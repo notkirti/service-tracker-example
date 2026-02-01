906bbb35851b4f7fd84e2490f3e3c07d1a25c958
Kirti <thirumurthy.kirti@gmail.com>
2026-02-02 07:36:33 +0800

    feat: successfully scaffolded job crud views and resolved ef core dependency issues

--- ServiceTrackerExample/obj/Debug/net10.0/ServiceTrackerExample.AssemblyInfo.cs
+++ ServiceTrackerExample/obj/Debug/net10.0/ServiceTrackerExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceTrackerExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c123d8f4c0b8b92fdc4d41878415dfe80342b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa65899aeac46e1c2e78873cea17b7f79b5b16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceTrackerExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceTrackerExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
